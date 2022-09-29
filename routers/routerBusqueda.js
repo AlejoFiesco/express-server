45e079fd4b3b0a3a71108ef99dfe547dd0be277e
AlejoFiesco <alfiesparra@hotmail.com>
2022-09-29 15:51:59 -0500

    updated get

--- routers/routerBusqueda.js
+++ routers/routerBusqueda.js
@@ -5,35 +5,30 @@ const { pool } = require('../pool/pool.js');
 //middleware
 routerBusquedas.use(express.json());
 
-routerBusquedas.get('/:tabla', (req, res) => {
-    const body = {
-        tabla: req.params.tabla,
-        columnas: req.params.columnas,
-        condiciones: req.params.condiciones
-    };
+routerBusquedas.get('/', (req, res) => {
+    res.send(JSON.stringify({
+        respuesta: "respuesta"
+    }))
+});
 
-    buscar(body)
+routerBusquedas.get('/:tabla', (req, res) => {
+    const tabla = req.params.tabla;
+
+    buscar(tabla)
         .then((response) => {
             res.send(response);
         });
 });
 
-const buscar = async ( body ) => {
-    if(body.query){
-        return buscarQuery(body.query);
-    };
-
-    if(body.tabla){
-        return buscarTabla(body.tabla, body.columnas, body.condiciones);
-    }
-    
-    return res.rows;
+const buscar = async ( tabla, columnas=undefined, condiciones=undefined ) => {  
+    return await buscarTabla(tabla, columnas, condiciones);
 }
 
 const buscarTabla = async ( tabla, columnas, condiciones ) => {
     const stringColumnas =  columnas === undefined ? '*' : columnas.join(', ');
     const stringCondiciones = condiciones === undefined ? '' : 'WHERE ' + condiciones;
     const query = `SELECT ${stringColumnas} FROM ${tabla} ${stringCondiciones}`;
+    console.log(query);
     const res = await pool.query(query);
     if(res.error){
         routerBusquedas.status(400).send('Error en el query');
